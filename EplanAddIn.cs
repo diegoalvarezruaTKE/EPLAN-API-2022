2f59076361d854b36e0cd1ad4bf3fac49152074d
diegoalvarezrua <diegoalvarezrua@gmail.com>
2024-08-29 18:57:44 +0200

    diego

--- EplanAddIn.cs
+++ EplanAddIn.cs
@@ -3,6 +3,7 @@ using Eplan.EplApi.Base;
 using Eplan.EplApi.DataModel;
 using static System.Net.Mime.MediaTypeNames;
 using System;
+using Eplan.EplApi.Gui;
 
 namespace EplanAddIn
 {
@@ -23,6 +24,11 @@ namespace EplanAddIn
         }
         public bool OnInitGui()
         {
+            var ribbonBar = new RibbonBar();
+            var ribbonTab = ribbonBar.AddTab("API 2022");
+            var ribbonCommandGroup = ribbonTab.AddCommandGroup("Config");
+            ribbonCommandGroup.AddCommand("Configurador", "ConfigAction");
+            ribbonCommandGroup.AddCommand("Lista de Materiales", "MaterialesAction");
             return true;
         }
         public bool OnExit()
