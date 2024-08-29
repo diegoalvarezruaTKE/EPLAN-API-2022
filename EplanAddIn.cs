1aa7e1a7dcc85f4b05b83e70fd73ea80a37d1db8
diegoalvarezrua <diegoalvarezrua@gmail.com>
2024-08-29 18:51:20 +0200

    Agregar archivos de proyecto.

---
+++ EplanAddIn.cs
@@ -0,0 +1,35 @@
+﻿using Eplan.EplApi.ApplicationFramework;
+using Eplan.EplApi.Base;
+using Eplan.EplApi.DataModel;
+using static System.Net.Mime.MediaTypeNames;
+using System;
+
+namespace EplanAddIn
+{
+    public class AddInModule : IEplAddIn
+    {
+        public bool OnRegister(ref System.Boolean bLoadOnStart)
+        {
+            bLoadOnStart = true;
+            return true;
+        }
+        public bool OnUnregister()
+        {
+            return true;
+        }
+        public bool OnInit()
+        {
+            return true;
+        }
+        public bool OnInitGui()
+        {
+            return true;
+        }
+        public bool OnExit()
+        {
+            return true;
+        }
+    }
+
+
+}
