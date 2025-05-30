f85981c9e1e16f7aac9f8edd0f3111fbacac6508
diegoalvarezrua <71273968+diegoalvarezrua@users.noreply.github.com>
2025-05-30 10:20:10 +0200

    30.5.25.02

--- API_Esquemas/DrawBasicArmIntEN.cs
+++ API_Esquemas/DrawBasicArmIntEN.cs
@@ -49,6 +49,7 @@ namespace EPLAN_API.User
             try
             {
                 //Hola Soy Adri
+                //Hola Soy Diego
                 draw_Default_Param();
                 progress += step;
                 ProgressChanged(progress);
